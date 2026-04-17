e7adce5462b1daa2469c111a193de79b64732458
Rodrigo Peruch <rodrigo@favalessa.com.br>
2026-04-17 12:08:41 -0300

    Fix macOS: quit app when window is closed instead of running headless

--- electron/main.ts
+++ electron/main.ts
@@ -177,11 +177,8 @@ app.whenReady().then(() => {
 
   createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 });
 
 app.on('window-all-closed', () => {
-  if (!isMac) app.quit();
+  app.quit();
 });
